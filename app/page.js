c6087ae86420b700b33535834d262bdf85213978
tohidbinazam <tohidbinazamsunny1@gmail.com>
2023-07-31 15:17:11 +0600

    get post done

--- app/page.js
+++ app/page.js
@@ -1,16 +1,19 @@
-import Image from 'next/image';
-import baby from '@/public/baby-boy-posing.webp';
+// import Image from 'next/image';
+// import baby from '@/public/baby-boy-posing.webp';
+
+import User from './user/page';
 
 export default function Home() {
   return (
     <>
-      <h1> Hello World </h1>
+      <User />
+      {/* <h1> Hello World </h1>
       <Image
         src={baby}
         width={'500'}
         height={'400'}
         alt='Picture of the author'
-      />
+      /> */}
     </>
   );
 }
